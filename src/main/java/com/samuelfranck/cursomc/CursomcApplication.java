1ae50ea8f8bb5b7c9a2b83f27fbed597c4eefc53
Samuel Franck <samuelfranck.adm@gmail.com>
2020-04-14 20:31:56 -0300

    Projeto criado

---
+++ src/main/java/com/samuelfranck/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.samuelfranck.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelfranck/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelfranck.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
