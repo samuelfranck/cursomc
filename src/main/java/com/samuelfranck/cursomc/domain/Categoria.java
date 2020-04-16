cd9da2e341e4820589a0c4c489779b4bff961cec
Samuel Franck <samuelfranck.adm@gmail.com>
2020-04-16 18:34:18 -0300

    Implementação do Banco de Dados H2 e criação automática da base de dados

--- src/main/java/com/samuelfranck/cursomc/domain/Categoria.java
+++ src/main/java/com/samuelfranck/cursomc/domain/Categoria.java
@@ -1,12 +1,20 @@
 package com.samuelfranck.cursomc.domain;
 
 import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 //import java.util.List;
 
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
